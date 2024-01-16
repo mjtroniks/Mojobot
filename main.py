989f148d4fceb6947450f44926a2734ebcabc0a9
philipo364 <felipeproano@hotmail.co.uk>
2024-01-16 13:47:47 +0000

    v2

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
+"""
+*****************************
+* Developer: MJtronics
+* Date: 2024-01-15
+*****************************
+
+Description:
+This code controls RGB LEDs, two motors, an ultrasonic sensor, and infrared sensors. It receives distance
+and infrared sensor measurements and adjusts the motor and LED actions based on the measured distance
+and sensor states. The actions are repeated in a loop.
+
+- If the distance is greater than 40 cm, the robot checks the infrared sensors:
+- If the right sensor is active and the left sensor is inactive, it displays a green color on the right LED, turns off the left LED, and stops the motors.
+- If the left sensor is active and the right sensor is inactive, it displays a green color on the left LED, turns off the right LED, and stops the motors.
+- If both sensors are inactive, it turns off both LEDs and stops the motors.
+- If both sensors are active, it turns off both LEDs and stops the motors.
+
+- If the distance is between 5 cm and 20 cm, the robot moves forward with green color on both LEDs.
+
+- If the distance is between 20 cm and 40 cm, the robot moves backward with red color on both LEDs.
+
+
+Pin Configuration:
+- Ultrasonic Sensor:
+  * Trigger Pin(GP14), Echo Pin(GP15)
+- RGB LEDs:
+  * Right LED: Red(GP22), Green(GP20), Blue(GP21)
+  * Left LED: Red(GP7), Green(GP9), Blue(GP8)
+- Motors:
+  * Motor 1: PWM Pin(GP10), Direction Pin(GP12)
+  * Motor 2: PWM Pin(GP11), Direction Pin(GP13)
+- Infrared Sensors:
+  * Left Infrared Sensor: GP2
+  * Right Infrared Sensor: GP3
+
+"""
+
 import machine
 from machine import Pin, PWM, time_pulse_us
 import utime
-##################Ultrasonic##############################
+
+################## Ultrasonic ##############################
 # Ultrasonic sensor pins
 trigger_pin = Pin(14, Pin.OUT)
 echo_pin = Pin(15, Pin.IN)
-#######################Leds###############################
+
+####################### LEDs ###############################
+# Define the RGB LED pins for the right RGB LED
 led_red_right = Pin(22)
 led_green_right = Pin(20)
 led_blue_right = Pin(21)
@@ -15,7 +55,7 @@ pwm_red_right = PWM(led_red_right)
 pwm_green_right = PWM(led_green_right)
 pwm_blue_right = PWM(led_blue_right)
 
-# Define the LED pins for the left RGB LED
+# Define the RGB LED pins for the left RGB LED
 led_red_left = Pin(7)
 led_green_left = Pin(9)
 led_blue_left = Pin(8)
@@ -33,7 +73,8 @@ pwm_blue_right.freq(frequency)
 pwm_red_left.freq(frequency)
 pwm_green_left.freq(frequency)
 pwm_blue_left.freq(frequency)
-###################Motors#######################################
+
+################### Motors #################################
 # Motor 1
 motor1_pwm_pin = Pin(10)
 motor1_dir_pin = Pin(12, machine.Pin.OUT)
@@ -43,57 +84,108 @@ motor1_pwm = PWM(motor1_pwm_pin, freq=1000)  # Explicitly set frequency for isol
 motor2_pwm_pin = Pin(11)
 motor2_dir_pin = Pin(13, machine.Pin.OUT)
 motor2_pwm = PWM(motor2_pwm_pin, freq=1000)  # Explicitly set frequency for isolation
-##################Infrared#########################
+
+################## Infrared #############################
 # Line following sensors
-sensor1_pin = Pin(2, Pin.IN)#left IR
-sensor2_pin = Pin(3, Pin.IN)#right IR
+infrared_left_pin = Pin(2, Pin.IN)  # left IR
+infrared_right_pin = Pin(3, Pin.IN)  # right IR
 
 # Previous sensor states
-prev_sensor1_state = sensor1_pin.value()
-prev_sensor2_state = sensor2_pin.value()
+prev_infrared_left_state = infrared_left_pin.value()
+prev_infrared_right_state = infrared_right_pin.value()
 
-#################Functions#############################
+################# Functions ##############################
 def map_user_input(user_input):
+    """
+    Maps user input to PWM range.
+
+    Args:
+    - user_input: User input value (0 to 255).
+
+    Returns:
+    - int: Mapped value in the PWM range (0 to 65535).
+    """
     # Ensure user_input is within the valid range
     user_input = max(0, min(255, user_input))
     # Map the value to the PWM range
     return int((user_input / 255) * 65535)
 
 def set_rgb_led(pwm_red, pwm_green, pwm_blue, r_value, g_value, b_value):
+    """
+    Sets RGB LED colors using PWM values.
+
+    Args:
+    - pwm_red: PWM object for the red LED.
+    - pwm_green: PWM object for the green LED.
+    - pwm_blue: PWM object for the blue LED.
+    - r_value: PWM value for the red color.
+    - g_value: PWM value for the green color.
+    - b_value: PWM value for the blue color.
+    """
     pwm_red.duty_u16(r_value)
     pwm_green.duty_u16(g_value)
     pwm_blue.duty_u16(b_value)
 
 def turn_off_leds(pwm_red, pwm_green, pwm_blue):
+    """
+    Turns off RGB LEDs.
+
+    Args:
+    - pwm_red: PWM object for the red LED.
+    - pwm_green: PWM object for the green LED.
+    - pwm_blue: PWM object for the blue LED.
+    """
     pwm_red.duty_u16(0)
     pwm_green.duty_u16(0)
     pwm_blue.duty_u16(0)
 
 def map_speed_to_pwm(speed):
+    """
+    Maps speed percentage to PWM range.
+
+    Args:
+    - speed: Motor speed percentage (0 to 100).
+
+    Returns:
+    - int: Mapped value in the PWM range (0 to 65535).
+    """
     # Ensure speed is within the valid range
     speed = max(0, min(100, speed))
     # Map the speed to the PWM range
     return int((speed / 100) * 65535)
 
 def forward():
+    """
+    Moves both motors forward.
+    """
     motor1_dir_pin.on()
     motor2_dir_pin.on()
     motor1_pwm.duty_u16(speed_pwm)
     motor2_pwm.duty_u16(speed_pwm)
 
 def backward():
+    """
+    Moves both motors backward.
+    """
     motor1_dir_pin.off()
     motor2_dir_pin.off()
     motor1_pwm.duty_u16(speed_pwm)
     motor2_pwm.duty_u16(speed_pwm)
 
 def stop_motors():
+    """
+    Stops both motors.
+    """
     motor1_pwm.duty_u16(0)
     motor2_pwm.duty_u16(0)
 
-
-
 def get_distance():
+    """
+    Measures the distance using an ultrasonic sensor.
+
+    Returns:
+    - int: The distance in centimeters.
+    """
     # Trigger pulse to start measurement
     trigger_pin.off()
     utime.sleep_us(2)
@@ -108,7 +200,8 @@ def get_distance():
     distance = (pulse_width / 2) / 29.1
 
     return round(distance)
-#####################Main Program######################
+
+# Main Program
 while True:
     try:
         distance_cm = get_distance()
@@ -119,9 +212,44 @@ while True:
         speed_pwm = map_speed_to_pwm(speed_percent)
         print(distance_cm)
         # Read current sensor states
-        current_sensor1_state = sensor1_pin.value()#left infrared sensor
-        current_sensor2_state = sensor2_pin.value()#right infrared sensor
-        print("Sensor left "+str(current_sensor1_state)+"  Sensor right "+str(current_sensor2_state))
+        current_infrared_left_state = infrared_left_pin.value()  # left infrared sensor
+        current_infrared_right_state = infrared_right_pin.value()  # right infrared sensor
+        print("Infrared left " + str(current_infrared_left_state) + "  Infrared right " + str(
+            current_infrared_right_state))
+
+        if distance_cm > 40:
+            if current_infrared_right_state == 1 and current_infrared_left_state == 0:
+                pwm_valueR = map_user_input(17)
+                pwm_valueG = map_user_input(236)
+                pwm_valueB = map_user_input(229)
+                set_rgb_led(pwm_red_right, pwm_green_right, pwm_blue_right, pwm_valueR, pwm_valueG,
+                            pwm_valueB)  # Green color
+                set_rgb_led(pwm_red_left, pwm_green_left, pwm_blue_left, 0, 0, 0)  # Green color
+                stop_motors()
+            if current_infrared_left_state == 1 and current_infrared_right_state == 0:
+                pwm_valueR = map_user_input(17)
+                pwm_valueG = map_user_input(236)
+                pwm_valueB = map_user_input(229)
+                set_rgb_led(pwm_red_right, pwm_green_right, pwm_blue_right, 0, 0, 0
+                            )  # Green color
+                set_rgb_led(pwm_red_left, pwm_green_left, pwm_blue_left, pwm_valueR, pwm_valueG, pwm_valueB)  # Green color
+                stop_motors()
+            if current_infrared_right_state == 0 and current_infrared_left_state == 0:
+                pwm_valueR = map_user_input(17)
+                pwm_valueG = map_user_input(236)
+                pwm_valueB = map_user_input(229)
+                set_rgb_led(pwm_red_right, pwm_green_right, pwm_blue_right, 0, 0,
+                            0)  # Green color
+                set_rgb_led(pwm_red_left, pwm_green_left, pwm_blue_left, 0, 0, 0)  # Green color
+                stop_motors()
+            if current_infrared_left_state == 1 and current_infrared_right_state == 1:
+                pwm_valueR = map_user_input(17)
+                pwm_valueG = map_user_input(236)
+                pwm_valueB = map_user_input(229)
+                set_rgb_led(pwm_red_right, pwm_green_right, pwm_blue_right, 0, 0, 0
+                            )  # Green color
+                set_rgb_led(pwm_red_left, pwm_green_left, pwm_blue_left, 0, 0, 0)  # Green color
+                stop_motors()
         if distance_cm > 5 and distance_cm < 20:
             # Forward
             pwm_value = map_user_input(255)
@@ -135,12 +263,6 @@ while True:
             set_rgb_led(pwm_red_right, pwm_green_right, pwm_blue_right, pwm_value, 0, 0)  # Red color
             set_rgb_led(pwm_red_left, pwm_green_left, pwm_blue_left, pwm_value, 0, 0)  # Red color
             backward()
-
-        else:
-            # Stop
-            turn_off_leds(pwm_red_right, pwm_green_right, pwm_blue_right)
-            turn_off_leds(pwm_red_left, pwm_green_left, pwm_blue_left)
-            stop_motors()
 
     except ValueError:
         print("Invalid input. Please enter a valid number.")
