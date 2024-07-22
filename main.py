01b1c251c6e4095ac2dd4b63009d739e32ba1999
Josh <jproano2023@Elam.co.uk>
2024-07-22 16:57:58 +0100

    29/05/2024

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+"""
+Test Program for Mojobot
+Author: MJtroniks
+Website: www.mjtroniks.com
+GitHub: https://github.com/mjtroniks/Mojobot
+
+This program is designed to verify that all hardware components of the Mojobot are functioning as expected.
+It performs the following tests:
+
+1. **Red LEDs and Ultrasonic Sensor**:
+   - Measures distance using the ultrasonic sensor.
+   - Adjusts the brightness of the red LEDs based on the distance measured.
+   - The red LEDs are at maximum brightness when the distance is 100 cm or more, and at minimum brightness when the distance is 10 cm or less.
+
+2. **Blue LEDs and Infrared Sensors**:
+   - Checks the infrared sensors to detect a black surface.
+   - Turns on the corresponding blue LED if a black surface is detected by the left or right infrared sensor.
+
+3. **Motors and Green LEDs**:
+   - Tests the left motor and green LED based on the distance measured.
+   - The left motor and green LED's speed and brightness vary from minimum (at 2 cm) to maximum (at 50 cm).
+   - Repeats the same test for the right motor and green LED after turning off the left motor and LED.
+
+This program can be implemented to verify that all hardware is working as expected.
+"""
+
 import machine
 import utime
 
@@ -21,7 +47,7 @@ motor1_dir = machine.Pin(12, machine.Pin.OUT)
 motor2_pwm = machine.PWM(machine.Pin(11))
 motor2_dir = machine.Pin(13, machine.Pin.OUT)
 
-# Set PWM frequency
+# Set PWM frequency for LEDs and motors
 led_left_red.freq(1000)
 led_left_green.freq(1000)
 led_left_blue.freq(1000)
@@ -31,7 +57,6 @@ led_right_blue.freq(1000)
 
 motor1_pwm.freq(1000)
 motor2_pwm.freq(1000)
-
 
 # Function to measure distance using ultrasonic sensor
 def measure_distance():
@@ -50,19 +75,26 @@ def measure_distance():
     distance = (time_passed * 0.0343) / 2
     return distance
 
-
 # Function to control LED brightness based on distance
 def control_red_leds_by_distance():
     print("Controlling Red LEDs by Distance")
+    led_left_red.duty_u16(0)
+    led_right_red.duty_u16(0)
+    led_left_blue.duty_u16(0)
+    led_right_blue.duty_u16(0)
+    led_left_green.duty_u16(0)
+    led_right_green.duty_u16(0)
+    motor1_pwm.duty_u16(0)
+    motor2_pwm.duty_u16(0)
     for _ in range(500):
         distance = measure_distance()
         print("Distance:", distance)
-        if distance <= 2:
+        if distance <= 5:
             pwm_value = 0
-        elif distance >= 20:
+        elif distance >= 40:
             pwm_value = 65535
         else:
-            pwm_value = int((distance / 20) * 65535)
+            pwm_value = int((distance / 30) * 65535)
 
         led_left_red.duty_u16(pwm_value)
         led_right_red.duty_u16(pwm_value)
@@ -71,10 +103,10 @@ def control_red_leds_by_distance():
     led_left_red.duty_u16(0)
     led_right_red.duty_u16(0)
 
-
 # Function to control blue LEDs based on infrared sensors
 def control_blue_leds_by_infrared():
     print("Controlling Blue LEDs by Infrared Sensors")
+
     for _ in range(500):
         left_value = infrared_left.value()
         right_value = infrared_right.value()
@@ -82,13 +114,11 @@ def control_blue_leds_by_infrared():
 
         if left_value == 1:
             led_left_blue.duty_u16(65535)
-
         else:
-
             led_left_blue.duty_u16(0)
+
         if right_value == 1:
             led_right_blue.duty_u16(65535)
-
         else:
             led_right_blue.duty_u16(0)
 
@@ -143,13 +173,13 @@ def control_motors_and_green_leds():
     utime.sleep(5)
     led_left_green.duty_u16(0)
     led_right_green.duty_u16(0)
+
 # Test sequence
 def test_robot():
     control_red_leds_by_distance()
     control_blue_leds_by_infrared()
     control_motors_and_green_leds()
 
-
 # Run the test
 while True:
- test_robot()
+    test_robot()
